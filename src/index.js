caf177254c53504e1b8a68c5f42f236cf49afe92
Matteo Piccini <gnappo.1@live.it>
2023-09-19 16:21:25 -0700

    End of walktrhough code

--- src/index.js
+++ src/index.js
@@ -1,87 +1,96 @@
+let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  const toyForm = document.querySelector(".add-toy-form");
-  const toyCollection = document.querySelector("#toy-collection")
-  let addToy = false;
+// document.addEventListener("DOMContentLoaded", () => {
+// });
+const addBtn = document.querySelector("#new-toy-btn");
+const toyCollection = document.querySelector("#toy-collection")
+const addNewToyForm = document.querySelector(".add-toy-form")
+const toyFormContainer = document.querySelector(".container");
 
-  const handleClick = (event) => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-
+addBtn.addEventListener("click", () => {
+  // hide & seek with the form
+  addToy = !addToy;
+  if (addToy) {
+    toyFormContainer.style.display = "block";
+  } else {
+    toyFormContainer.style.display = "none";
   }
-
-  const handleSubmit = (event) => { 
-    event.preventDefault()
-    toy = {}
-    // toy.id = toyCollection.childNodes.length + 1
-    toy.name = event.target.name.value
-    toy.image = event.target.image.value
-    toy.likes = 0
-    // renderToy(toy)
-
-    const configObj = {
-      method: "post",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(toy)
-    }
-
-    fetch("http://localhost:3000/toys", configObj)
-    .then(response => response.json())
-    .then(newToy => renderToy(newToy))
-    .catch(error => alert(error))
-
-
-    event.target.reset()
-  }
-
-  addBtn.addEventListener("click", handleClick);
-  toyForm.addEventListener("submit", handleSubmit);
-
-  const renderToy = (toyObj) => {
-    // create the DOM elements that each toy card should have
-    const toyDiv = document.createElement("div")
-    const toyName = document.createElement("h2")
-    const toyImg = document.createElement("img")
-    const toyLikes = document.createElement("p")
-    const likeBtn = document.createElement("button")
-
-    // assign properties to the elements
-    // toyDiv.className += " card"
-    toyDiv.classList.add("card")
-    toyName.innerText = toyObj.name
-    toyImg.src = toyObj.image
-    toyImg.classList.add("toy-avatar")
-    toyLikes.innerText = `${toyObj.likes} likes`
-    likeBtn.id = toyObj.id
-    likeBtn.classList.add("like-btn")
-    likeBtn.innerText = "Like"
-
-    toyDiv.append(toyName, toyImg, toyLikes, likeBtn)
-    toyCollection.appendChild(toyDiv)
-
-    // toyDiv.appendChild(toyImg)
-    // toyDiv.appendChild(toyLikes)
-    // toyDiv.appendChild(likeBtn)
-  }
-
-  const fetchData = () => {
-    fetch("http://localhost:3000/toys")
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      data.forEach(renderToy)
-    })
-  }
-
-  fetchData()
-
 });
+//! New code here
+
+const URL = "http://localhost:3000/toys"
+
+const createToyCard = (toyInfo) => {
+  const toyCard = document.createElement("div")
+  toyCard.className = "card"
+  toyCard.id = `toy-${toyInfo.id}`
+  const h2 = document.createElement("h2")
+  h2.textContent = toyInfo.name
+  const img = document.createElement("img")
+  img.src = toyInfo.image
+  img.alt = toyInfo.name
+  img.className = "toy-avatar"
+  const p = document.createElement("p")
+  p.textContent = `${toyInfo.likes} Likes`
+  const btn = document.createElement("button")
+  btn.textContent = "Like ❤️"
+  btn.className = "like-btn"
+  btn.id = toyInfo.id
+  btn.addEventListener("click", event => {
+    fetch(`${URL}/${toyInfo.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({likes: ++toyInfo.likes})
+    })
+    .then(response => response.json())
+    .then(updatedToy => {
+      event.target.parentElement.querySelector("p").textContent = `${updatedToy.likes} Likes`
+    })
+    .catch(error => alert(error))
+  })
+
+  toyCard.append(h2, img, p, btn)
+  toyCollection.append(toyCard)
+
+  // toyCollection.innerHTML += `
+  //   <div class="card">
+  //     <h2>${toyInfo.name}</h2>
+  //     <img src="${toyInfo.image}" alt="${toyInfo.name}" class="toy-avatar" />
+  //     <p>${toyInfo.likes} Likes</p>
+  //     <button class="like-btn" id="${toyInfo.id}">Like ❤️</button>
+  // </div>
+  // `
+
+}
+//! Fetch all toys
+
+fetch(URL)
+.then(response => response.json())
+.then(toysData => toysData.forEach(createToyCard))
+.catch(error => alert(error))
+
+//! Start working on form
+addNewToyForm.addEventListener("submit", function(event){
+  event.preventDefault()
+  const name = event.target.name.value
+  const image = event.target.image.value
+  if (name.trim() && image.trim()) { //! If the user inputted data
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({name, image, likes:0})
+    })
+    .then(response => response.json())
+    .then(newToyCreated => {
+      createToyCard(newToyCreated)//! make sure you use the toy from the second then!!! It will also have an id!!!
+      event.target.reset()
+    }) 
+    .catch(error => alert(error))
+  } else {
+    alert("Please fill out the whole form!")
+  }
+})
