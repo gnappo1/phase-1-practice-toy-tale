259201908d9b8a3f344bac832c2304053df9f7a5
Matteo Piccini <gnappo.1@live.it>
2025-01-14 14:59:29 -0800

    complete version 2 creating the nodes manually

--- src/index.js
+++ src/index.js
@@ -1,34 +1,53 @@
-{/* <div class="card">
-  <h2>Woody</h2>
-  <img src="[toy_image_url]" class="toy-avatar" />
-  <p>4 Likes</p>
-  <button class="like-btn" id="[toy_id]">Like ❤️</button>
-</div> */}
 
 
 // document.addEventListener("DOMContentLoaded", () => {
   //! Global Variables
-const addBtn = document.querySelector("#new-toy-btn");
-const toyFormContainer = document.querySelector(".container");
-const toyCollection = document.querySelector("#toy-collection");
-const toyForm = document.querySelector("form")
-
-
-
-//! Helper Functions
-//! we create an async func to wrap our fetch logic
-//! best practice: try..catch statement inside
-const fetchData = async () => {
-  try {
-    const response = await fetch("http://localhost:3000/toys")
-    const data = await response.json()
-    return data
-  } catch (error) {
-    alert(error)
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection");
+  const toyForm = document.querySelector("form")
+  
+  
+  
+  //! Helper Functions
+  //! we create an async func to wrap our fetch logic
+  //! best practice: try..catch statement inside
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/toys")
+      const data = await response.json()
+      return data
+    } catch (error) {
+      alert(error)
+    }
   }
+  
+const displayToy = (toy) => {
+  const div = document.createElement("div")
+  div.className = "card"
+
+  const h2 = document.createElement("h2")
+  h2.innerText = toy.name
+  
+  const img = document.createElement("img")
+  img.className = "toy-avatar"
+  img.src = toy.image
+  img.alt = toy.name
+
+  const p = document.createElement("p")
+  p.innerText = `${toy.likes} Likes`
+  
+  const button = document.createElement("button")
+  button.className = "like-btn"
+  button.id = toy.id
+  button.innerText = "Like ❤️"
+  button.addEventListener("click", () => {
+    p.innerText = `${++toy.likes} Likes`
+  })
+
+  div.append(h2, img, p, button)
+  toyCollection.append(div)
 }
-
-
 //! Attach Listeners
 
 addBtn.addEventListener("click", () => {
@@ -66,14 +85,7 @@ toyForm.addEventListener("submit", (e) => {
       //! Extract the json data out of the Response body
       const data = await response.json()
       //! Append the newly created data onto the page
-      toyCollection.innerHTML += `
-        <div class="card">
-          <h2>${data.name}</h2>
-          <img src=${data.image} class="toy-avatar" alt=${data.name}/>
-          <p>${data.likes} Likes</p>
-          <button class="like-btn" id=${data.id}>Like ❤️</button>
-        </div>
-      `
+      displayToy(data)
       e.target.reset()
     } catch (error) {
       alert(error)
@@ -86,23 +98,6 @@ toyForm.addEventListener("submit", (e) => {
 
 //! Invoke logic
 fetchData("http://localhost:3000/toys")
-.then(toys => toys.forEach(toy => {
-  toyCollection.innerHTML += `
-    <div class="card">
-      <h2>${toy.name}</h2>
-      <img src=${toy.image} class="toy-avatar" alt=${toy.name}/>
-      <p>${toy.likes} Likes</p>
-      <button class="like-btn" id=${toy.id}>Like ❤️</button>
-    </div>
-  `
-}))
-.then(() => {
-  const allToyLikeButtons = document.querySelectorAll("div.card button")
-  allToyLikeButtons.forEach(button => button.addEventListener("click", (e) => {
-    let paragraph = e.target.previousElementSibling
-    let likesNum = parseInt(paragraph.innerText.replace(" Likes", ""))
-    paragraph.innerText = `${++likesNum} Likes`
-  }))
-})
+  .then(toys => toys.forEach(toy => displayToy(toy)))
 
 
